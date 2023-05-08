9aad146b975f6c39dd4ee97b223aa145a763c4a4
hadjuse <rabearimananahadjpro@gmail.com>
2023-05-08 23:00:00 +0200

    correction des connexions

--- php/Jeune.php
+++ php/Jeune.php
@@ -14,14 +14,14 @@
         // verifie si l'utilisateur existe et initialie une session et un cookie pour l'utilisateurs
         $utilisateurs_trouve = false;
         foreach($utilisateurs as $utilisateur){
-            if ($mail == $utilisateur['mail'] && $mdp == $utilisateur['mdp']){
-                $_SESSION['login'] = $utilisateur['nom'];      
+            if ($mail === $utilisateur['mail'] && $mdp === $utilisateur['mdp']){
+                $_SESSION['login'] = $utilisateur['prenom'];      
                 setcookie("login", $utilisateur["prenom"], time()+60*60*24);
                 break;
             }
         }
         //$response = array('connexion' => $utilisateurs_trouve);
-        //echo json_encode($response);
+        //echo json_encode($response); 
     }
 ?>
 <!DOCTYPE html>
@@ -31,7 +31,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PageJeune</title>
-    <link rel="stylesheet" href="../css/inscription.css">
+    <link rel="stylesheet" href="../css/pagejeune.css">
 </head>
 <body>
 <!--banderole avec le nom de la page que la quelle on se situe-->
@@ -42,15 +42,17 @@
         </div>
         <!--onglet de navigation entre les differentes pages-->
             <div id="navigation">
-                <a href="pagejeune.html" id="jeunes">JEUNES</a>
-                <a href="pagereferent.html" id="referent">RÉFÉRENT</a>
-                <a href="pageconsultant.html" id="consultant">CONSULTANT</a>
-                <a href="pagepartenaire.html" id="partenaires">PARTENAIRES</a>
+                <a href="../php/Jeune.php" id="jeunes">JEUNES</a>
+                <a href="../pagereferent.html" id="referent">RÉFÉRENT</a>
+                <a href="../pageconsultant.html" id="consultant">CONSULTANT</a>
+                <a href="../pagepartenaire.html" id="partenaires">PARTENAIRES</a>
             </div>
             <h1 align="center">Décrivez votre expérience et mettez en avant ce que vous en avez retiré.</h1>
-    <?php
-    $session_utilisateur = $_SESSION['login'];      
-    echo $_COOKIE['login'];
-    ?>
+    <h1>
+        <?php
+        $session_utilisateur = $_SESSION['login'];      
+        echo $session_utilisateur;
+        ?>
+    </h1>
 </body>
 </html>
