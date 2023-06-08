8d58cc2d548e1ed8739dbc32bbd42ae49a325b66
Flavio <soares.flavio2002@gmail.com>
2023-06-08 12:45:24 +0200

    verif mail non fonctionnel
    non fonctionnel

--- javascript/inscription.js
+++ javascript/inscription.js
@@ -1,18 +1,49 @@
 function verif_mail() {
-  alert("la");
-  fetch('../php/utilisateurs.json')
+  fetch('php/utilisateurs.json')
   .then(response => response.json())
   .then(data => {
     var utilisateurs = data['utilisateurs'];
     var mail = document.getElementById("mail").value;
-    forEach(utilisateurs => {
-      alert("la");
-      if (utilisateurs["mail"] == mail){
-        document.getElementById("mailfaux")="email existe deja";
+    alert(mail);
+    if(utilisateurs == null ){
+      alert('null');
+      return true;
+    }
+    else {
+      utilisateurs.foreach(utilisateur => {
+      alert(mail,"la");
+      if (utilisateur["mail"] == mail){
+        document.getElementById("mailfaux").value="email existe deja";
         return false;
       }
-    });
-    return false;
+      })
+    }
+    alert("la");
+    return true;
   })
-  .catch(error => console.error(error));
+}
+
+function verif_mail() {
+  fetch('php/utilisateurs.json')
+  .then(response => response.json())
+  .then(data => {
+      alert('a');
+    var utilisateurs = data['utilisateurs'];
+    var mail = document.getElementById("mail").value;
+    if(utilisateurs == null ){
+      var a = 0;
+      alert('b');
+    }
+    else {
+      alert('c');
+      utilisateurs.foreach(utilisateur => {
+      alert('d');
+      if (utilisateur["mail"] == mail){
+        document.getElementById("mailfaux")="email existe deja";
+        var a =1;
+      }
+      }) ;
+    }
+   var a =0;
+  })
 }
