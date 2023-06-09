49e2abe8ef4919c161949bb5ac7963c1411d7985
Flavio <soares.flavio2002@gmail.com>
2023-06-09 16:00:39 +0200

    a

--- php/modification_jeune.php
+++ php/modification_jeune.php
@@ -3,7 +3,7 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'php/vendor/autoload.php';
+    //require 'php/vendor/autoload.php';
     session_destroy();
     session_start();
     if (isset($_POST['submit'])) {
@@ -45,7 +45,7 @@
     
         try {
         // Envoi de l'e-mail
-        $mail->send();
+        //$mail->send();
         echo "L'e-mail a été envoyé avec succès.";
         } catch (Exception $e) {
             echo "Une erreur s'est produite lors de l'envoi de l'e-mail : " . $mail->ErrorInfo;
