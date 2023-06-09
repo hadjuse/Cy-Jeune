3d9c13e49ea572b6ec5874baa514a9dc80dcb424
hadjuse <rabearimananahadjpro@gmail.com>
2023-06-09 14:42:30 +0200

    ajout de de admin

--- admin.php
+++ admin.php
@@ -1,57 +1,40 @@
 <?php
     session_start();
-    
-    if (isset($_POST['submit']))
-    {
+
+    if (isset($_POST['submit'])) {
         $admin = $_POST['admin'];
-        $mdp= $_POST['mdp'];
-
-        // on déclare la function supprimer qu'on utilisera plus tards
-        function supprimer($id)
-        {
-            $jsonData = file_get_contents('php/utilisateurs.json'); // Charger les données JSON depuis le fichier
-            $data = json_decode($jsonData, true); // Décoder les données JSON en un tableau associatif
-
-            $cle_utilisateur = $id; // Clé du compte à supprimer
-            $index = -1; // Index du compte dans le tableau des données JSON
-
-            foreach ($data['utilisateurs'] as $cle => $account) { // Parcourir chaque compte
-                if ($account['indice'] === $cle_utilisateur) { // Vérifier si la clé correspond
-                    $index = $cle; // Stocker l'index du compte à supprimer
-                    break;
-                }
-            }
-
-            if ($index !== -1) { // Si l'index du compte a été trouvé
-                unset($data['utilisateurs'][$index]); // Supprimer le compte du tableau
-                $data['utilisateurs'] = array_values($data['utilisateurs']); // Réorganiser les index du tableau
-            }
-
-            $jsonData = json_encode($data, JSON_PRETTY_PRINT); // Encoder les données au format JSON
-            file_put_contents('php/utilisateurs.json', $jsonData); // Enregistrer les modifications dans le fichier
-
-        }
-
-        //Etape de connexion
+        $mdp = $_POST['mdp'];
+        // Étape de connexion
         $json_data = file_get_contents('php/admin.json');
-
-        // l'argument true permet de décoder le fichier en tant que tableau associative
-        $data = json_decode($json_data, true); 
-
+        // L'argument true permet de décoder le fichier en tant que tableau associatif
+        $data = json_decode($json_data, true);
         $administration = $data['admin'];
         $admin_trouver = false;
-        if ($administration[0]['pseudo'] == $admin && $administration[0]['mdp'] == $mdp){
+    
+        if ($administration[0]['pseudo'] == $admin && $administration[0]['mdp'] == $mdp) {
             $admin_trouver = true;
-            $_SESSION['admin'] = $admin; 
+            $_SESSION['admin'] = $admin;
         }
-        if ($admin_trouver == false){
+    
+        if ($admin_trouver == false) {
             header('Location: connexion_admin.html');
             exit;
         }
-        
-        //test
-        //supprimer(0);
+    
+        //include("php/suppresion.php");
+        //$s=$_GET['q'];
+        /*
+        echo $s;
+        print_r($_GET);
+        if (isset($s)) {
+            $ind = $s;
+            echo $ind;
+            // Faites d'autres opérations avec $ind si nécessaire
+        } else {
+            echo 'Indice non trouvé';
+        }*/
     }
+    
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -63,6 +46,7 @@
 </head>
 <body>
     <div id = "utilisateurs"></div>
+    <div id = "test"></div>
     <script src="javascript/admin.js"></script>
     <div class="btn-grad2"><a href="php/deconnexion.php" id="nouv"><div class="white">Déconnexion</div></a></div>
     
--- javascript/admin.js
+++ javascript/admin.js
@@ -1,15 +1,44 @@
-function afficherUtilisateurs(data){
-    var utilisateursDiv = document.getElementById("utilisateurs");
-    data.utilisateurs.forEach(function(utilisateur) {
-        // créer un élément <p> pour chaque utilisateurs
-        var utilisateurElement = document.createElement("p");
-        utilisateurElement.textContent = "Nom: " + utilisateur.nom + ", Prénom: " + utilisateur.prenom + ", Email: " + utilisateur.mail;
-        // ajout pour chaque nouvelles utilisateurs
-        utilisateursDiv.appendChild(utilisateurElement);
-    });
+  
+
+function afficherUtilisateurs(data) {
+  var utilisateursDiv = document.getElementById("utilisateurs");
+  data.utilisateurs.forEach(function(utilisateur, indice) {
+      // Créer un élément <p> pour chaque utilisateur et un bouton supprimer
+      var utilisateurElement = document.createElement("p");
+      var button = document.createElement('button');
+      button.type = 'button';
+      button.innerHTML = 'Supprimer';
+      button.className = "supp_utilisateur";
+      utilisateurElement.textContent = "Nom: " + utilisateur.nom + ", Prénom: " + utilisateur.prenom + ", Email: " + utilisateur.mail;
+      button.addEventListener('click', function() {
+            // mise à jour bdd
+            var ind = utilisateur.indice;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "../php/suppresion.php?q=" + ind, true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+            xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                console.log(ind + " envoyé avec succès au script PHP !");
+                console.log("Réponse du serveur : " + xhr.responseText);
+                } else {
+                console.log("Une erreur s'est produite lors de l'envoi du texte :", xhr.status);
+                }
+            }
+            };
+            location.reload();
+            xhr.send();
+      });
+      // Ajout pour chaque nouvel utilisateur
+      utilisateursDiv.appendChild(utilisateurElement);
+      utilisateursDiv.appendChild(button);
+  });
 }
+
+
 // Récupérer les données JSON à partir du fichier local
-fetch("php/utilisateurs.json")
+fetch("../php/utilisateurs.json")
         .then(function(response){
             return response.json();
         })
